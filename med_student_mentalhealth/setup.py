7275d169a6f1627384732143082ef2769be0c3ac
whh96 <66960104+whh96@users.noreply.github.com>
2023-04-27 11:26:27 -0500

    Add files via upload

---
+++ med_student_mentalhealth/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project provides insights into the wellbeing and performance of men and women in medical school.',
+    author='William Hampton Hoover',
+    license='',
+)
